5f59f95e868b7ed001a09932ca1b83fa78b08fbe
Bobby Langley <bobbylangley227@gmail.com>
2021-04-10 14:32:19 -0400

    updated api points to reflect most recent additions

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const {getEvents, postEvent, deleteEvent, updateEvent, getUserEvents} = require("./src/events/index");
+const { getEvents, postEvent, deleteEvent, updateEvent, getUserEvents, deleteUserEvent, getSingleEvent } = require("./src/events/index");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,10 +10,12 @@ app.use(cors());
 
 app.get("/events", getEvents);
 app.post("/events", postEvent);
-app.patch('/events/:eventId', updateEvent)
-app.delete('/events/:eventId', deleteEvent)
+app.patch("/events/:eventId", updateEvent);
+app.delete("/events/:eventId", deleteEvent);
 
-app.get('/events/:userId', getUserEvents)
+app.get("/events/:userId", getUserEvents);
+app.delete("/events/:userId/:eventId", deleteUserEvent);
 
+app.get("/event/:eventId", getSingleEvent);
 
 exports.app = functions.https.onRequest(app);
--- functions/src/events/index.js
+++ functions/src/events/index.js
@@ -4,24 +4,25 @@ var serviceAccount = require("../../credentials.json");
 let db;
 
 function dbAuth() {
-    if (!admin.apps.length) {
-      admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
     });
-      db = admin.firestore();
-    }
+    db = admin.firestore();
+  }
 }
 exports.getEvents = (req, res) => {
   // if(!req.params.userId){
   //     res.status(400).send('Invalid Request')
   // }
   dbAuth();
-//   .where('userId', '==', req.params.userId)
-  db.collection('events').get()
-    .then(collection => {
-      const eventsList = collection.docs.map(doc => {
-        let event = doc.data()
-        event.id = doc.id
+  //   .where('userId', '==', req.params.userId)
+  db.collection("events")
+    .get()
+    .then((collection) => {
+      const eventsList = collection.docs.map((doc) => {
+        let event = doc.data();
+        event.id = doc.id;
         return event;
       });
       res.status(200).json(eventsList);
@@ -29,17 +30,15 @@ exports.getEvents = (req, res) => {
     .catch((err) => res.status(500).send("GET EVENTS FAILED: " + err));
 };
 
-
-
 exports.postEvent = (req, res) => {
   // if (!req.body || !req.body.item || !req.body.userId || !req.params.userId) {
   //   res.status(400).send("Invalid request");
   // }
   dbAuth();
-  let newEvent = req.body
-  let now = admin.firestore.FieldValue.serverTimestamp()
-  newEvent.updated = now 
-  newEvent.created = now
+  let newEvent = req.body;
+  let now = admin.firestore.FieldValue.serverTimestamp();
+  newEvent.updated = now;
+  newEvent.created = now;
 
   db.collection("events")
     .add(newEvent)
@@ -47,35 +46,41 @@ exports.postEvent = (req, res) => {
       this.getEvents(req, res);
     })
     .catch((err) => res.status(500).send("POST FAILED" + err));
-    
 };
 
 exports.updateEvent = (req, res) => {
-  if (!req.body || !req.params.eventId ) {
+  if (!req.body || !req.params.eventId) {
     res.status(400).send("Invalid request");
   }
   dbAuth();
-  db.collection("events").doc(req.params.eventId).update(req.body)
-      .then(() => this.getEvents(req,res))
-      .catch(err => res.status(500).send('UPDATE FAILED: '+ err))
-}
+  db.collection("events")
+    .doc(req.params.eventId)
+    .update(req.body)
+    .then(() => this.getSingleEvent(req, res))
+    .catch((err) => res.status(500).send("UPDATE FAILED: " + err));
+};
 
 exports.deleteEvent = (req, res) => {
-    if(!req.params.eventId) {
-      res.status(400).send('Invalid request')
-    }
-    dbAuth()
-    db.collection('events').doc(req.params.eventId).delete()
-      .then(() => this.getEvents(req, res))
-      .catch(err => res.status(500).send('DELETE FAILED: '+ err))
-}
-
-exports.getUserEvents = (req, res) => {
-  if(!req.params.userId){
-      res.status(400).send('Invalid Request')
+  if (!req.params.eventId) {
+    res.status(400).send("Invalid request");
   }
   dbAuth();
-  db.collection("events").where('userId', '==', req.params.userId).get()
+  db.collection("events")
+    .doc(req.params.eventId)
+    // .where("userId", "==", req.params.userId)
+    .delete()
+    .then(() => this.getEvents(req, res))
+    .catch((err) => res.status(500).send("DELETE FAILED: " + err));
+};
+
+exports.getUserEvents = (req, res) => {
+  if (!req.params.userId) {
+    res.status(400).send("Invalid Request");
+  }
+  dbAuth();
+  db.collection("events")
+    .where("userId", "==", req.params.userId)
+    .get()
     .then((collection) => {
       const usersEvents = collection.docs.map((doc) => {
         let event = doc.data();
@@ -85,4 +90,42 @@ exports.getUserEvents = (req, res) => {
       res.status(200).send(usersEvents);
     })
     .catch((err) => res.status(500).send("GET TASKS FAILED: " + err));
-}
+};
+
+exports.deleteUserEvent = (req, res) => {
+  if (!req.params.eventId || !req.params.userId) {
+    res.status(400).send("Invalid request");
+  }
+  dbAuth();
+  db.collection("events")
+    .doc(req.params.eventId)
+    .delete()   
+    .then(() => this.getUserEvents(req, res))
+    .catch((err) => res.status(500).send("DELETE FAILED: " + err));
+};
+
+exports.getSingleEvent = (req, res) => {
+  if (!req.params.eventId) {
+    res.status(400).send("Invalid Request, no eventId");
+  }
+  console.log(req.params.eventId);
+  dbAuth();
+  //   .where('userId', '==', req.params.userId)
+  db.collection("events")
+    .doc(req.params.eventId)
+    .get()
+    .then((doc) => {
+      let event = doc.data();
+      event.id = doc.id;
+      res.status(200).json({
+        status: 'success',
+        data: event,
+        message: 'Events loaded successfully',
+        statusCode: 200
+      })
+
+      res.status(200).json(event);
+    })
+
+    .catch((err) => res.status(500).send("GET EVENT FAILED: " + err));
+};
